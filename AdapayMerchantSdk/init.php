041772b92ad8885c740627f24f6cb51f298ea7b8
root <17621853067@163.com>
2022-12-12 18:03:31 +0800

    add AdapayMerchantSdk

--- AdapayMerchantSdk/init.php
+++ AdapayMerchantSdk/init.php
@@ -1,14 +1,14 @@
 <?php
 define("SDK_BASE1", dirname(__FILE__));
-//define("ADAPAY_CORE", SDK_BASE . "/../AdapayCore");
+define("ADAPAY_CORE1", SDK_BASE . "/../AdapayCore");
 //define("SDK_VERSION", "v1.1.2");
 //define("GATE_WAY_URL", "https://%s.adapay.tech");
 //define("DEBUG", true);
 //define("LOG", dirname(SDK_BASE) . "/log/prod");
 //define("ENV", "prod");
 
-include_once ADAPAY_CORE . "/AdaPay.php";
-include_once ADAPAY_CORE . "/AdaLoader.php";
+include_once ADAPAY_CORE1 . "/AdaPay.php";
+include_once ADAPAY_CORE1 . "/AdaLoader.php";
 
 include_once SDK_BASE1 . "/MerchantConf.php";
 include_once SDK_BASE1 . "/MerchantUser.php";

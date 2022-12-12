4575f71953132b229a03fe1fe93cb6159e2cd6b8
root <17621853067@163.com>
2022-12-13 00:49:23 +0800

    add AdapayMerchantSdk

--- AdapayMerchantSdk/init.php
+++ AdapayMerchantSdk/init.php
@@ -1,16 +1,22 @@
 <?php
-define("SDK_BASE1", dirname(__FILE__));
-define("ADAPAY_CORE1", SDK_BASE . "/../AdapayCore");
+if (!defined('SDK_BASE')) {
+    define("SDK_BASE", dirname(__FILE__));
+}
+
+if (!defined('ADAPAY_CORE')) {
+    define("ADAPAY_CORE", SDK_BASE . "/../AdapayCore");
+}
+
 //define("SDK_VERSION", "v1.1.2");
 //define("GATE_WAY_URL", "https://%s.adapay.tech");
 //define("DEBUG", true);
 //define("LOG", dirname(SDK_BASE) . "/log/prod");
 //define("ENV", "prod");
 
-include_once ADAPAY_CORE1 . "/AdaPay.php";
-include_once ADAPAY_CORE1 . "/AdaLoader.php";
+include_once ADAPAY_CORE . "/AdaPay.php";
+include_once ADAPAY_CORE . "/AdaLoader.php";
 
-include_once SDK_BASE1 . "/MerchantConf.php";
-include_once SDK_BASE1 . "/MerchantUser.php";
-include_once SDK_BASE1 . "/MerchantProfile.php";
-include_once SDK_BASE1 . "/MerchantApp.php";
+include_once SDK_BASE . "/MerchantConf.php";
+include_once SDK_BASE . "/MerchantUser.php";
+include_once SDK_BASE . "/MerchantProfile.php";
+include_once SDK_BASE . "/MerchantApp.php";
